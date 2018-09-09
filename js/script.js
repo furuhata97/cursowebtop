8e7de1d88f1e6c48e1a8c1908be03c8006ee57f1
Gustavo Furuhata <gustavo_furuhata@hotmail.com>
2018-09-09 09:14:41 -0300

    correcao lista do menu e cor do botao

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@ $(function () {
     $(document).scroll(function () {
         var $nav = $(".navbar-fixed-top");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-        $("#logo>img").remove();
     });
 });
+
+
+$(function () {
+    $("#nav-button").click(function () {
+        var $nav = $("#nav-main");
+        var $nav2 = $(".navbar-fixed-top");
+        if ($($nav).hasClass("collapse-background")) {
+			$nav.removeClass('collapse-background');
+			$nav2.height(70);
+        }
+        else{
+        	$nav2.height(200);	
+        	$nav.toggleClass('collapse-background');
+        }
+    });
+});
+ 
