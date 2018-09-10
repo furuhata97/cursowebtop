0c2fe3579d2381e5330683015f354215403f7bff
Leonardo <ra98995@uem.br>
2018-09-09 23:48:43 -0300

    particlejs working in all parts

---
+++ app2.js
@@ -0,0 +1,3 @@
+particlesJS.load('particles-js2', 'particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
---
+++ app3.js
@@ -0,0 +1,3 @@
+particlesJS.load('particles-js3', 'particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
